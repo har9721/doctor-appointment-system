f2daf5d23b58357d1ed361c95ebe230b051ae21b
harshal chaudhary <harshal.chaudhary@e-stonetech.com>
2025-03-01 20:58:42 +0530

    add a validation for mark complete appointment

--- app/Http/Requests/AppointmentRequest.php
+++ app/Http/Requests/AppointmentRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class AppointmentRequest extends FormRequest
 {
@@ -16,7 +18,17 @@ class AppointmentRequest extends FormRequest
     public function rules()
     {
         return [
-            'appointment_date' => ['required','date_format:d-m-Y','after_or_equal:'.date('d-m-Y')],
+            'appointment_date' => ['required','date_format:d-m-Y',
+                function($attributes,$value,$fail)
+                {
+                    $currentDate = Carbon::now()->format('d-m-Y');
+
+                    if($value >= $currentDate)
+                    {
+                        $fail("The appointment cannot be marked as complete before the scheduled appointment date.");
+                    }
+                }
+            ],
             'patient_ID' => 'required',
             'reason' => 'nullable'
         ];
